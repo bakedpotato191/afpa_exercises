24a49f92d225318ca5b9e15977c45f73e94908f3
bakedpotato191 <vladislavp@pm.me>
2021-01-22 13:33:44 +0100

    Corrections

--- 22-01/tp_gallery/style.js
+++ 22-01/tp_gallery/style.js
@@ -2,11 +2,12 @@ function displayImage(link) {
   var path = link.src;
   var miniatures = document.getElementsByClassName('miniature');
   document.getElementsByClassName('container-mainimage')[0].innerHTML = "<img class='fullscreen' src='" + path + "' alt='avengers'>";
+  document.getElementsByClassName('container-mainimage')[0].style.border = "8px solid black";
   for (var i = 0; i < miniatures.length; i++) {
     if (path == miniatures[i].src) {
-      link.style.border = "2px solid red";
+      link.style.border = "4px solid red";
     } else {
-      miniatures[i].style.border = "2px solid black";
+      miniatures[i].style.border = "4px solid black";
     }
   }
 }
