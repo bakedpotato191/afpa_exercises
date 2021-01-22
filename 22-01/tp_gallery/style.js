b6679e94985cd99bc128f77c00c7048f7c4e03b1
bakedpotato191 <vladislavp@pm.me>
2021-01-22 13:21:04 +0100

    Affice l'image principal

--- 22-01/tp_gallery/style.js
+++ 22-01/tp_gallery/style.js
@@ -1,8 +1,7 @@
 function displayImage(link) {
-
   var path = link.src;
   var miniatures = document.getElementsByClassName('miniature');
-
+  document.getElementsByClassName('container-mainimage')[0].innerHTML = "<img class='fullscreen' src='" + path + "' alt='avengers'>";
   for (var i = 0; i < miniatures.length; i++) {
     if (path == miniatures[i].src) {
       link.style.border = "2px solid red";
