f632a1665668c37a44d81fb002ddfcfb19b82d05
bakedpotato191 <vladislavp@pm.me>
2021-01-29 12:50:57 +0100

    finalisation finale

--- 29-01/script.js
+++ 29-01/script.js
@@ -4,12 +4,7 @@ document.querySelectorAll('.tva').forEach(item => {
       document.getElementById('input-tva').value = item.textContent;
     } else {
       document.getElementById('input-tva').value = item.textContent;
-      console.log(parseInt(document.querySelector('.montant-hors-taxes').value));
-      document.querySelector('.montant-tva').value = (parseInt(document.querySelector('.montant-hors-taxes').value)) / 100 * parseFloat(item.textContent);
-      document.querySelector('.montant-ttc').value = (parseInt(document.querySelector('.montant-hors-taxes').value)) + parseFloat(document.querySelector('.montant-tva').value);
-      document.querySelector('.bottom-hors-taxes').textContent = document.querySelector('.montant-hors-taxes').value + "$";
-      document.querySelector('.bottom-tva').textContent = document.querySelector('.montant-tva').value + "$";
-      document.querySelector('.bottom-ttc').textContent = document.querySelector('.montant-ttc').value + "$";
+      updateValues();
     }
   })
 });
@@ -17,8 +12,8 @@ document.querySelectorAll('.tva').forEach(item => {
 document.querySelector('.montant-hors-taxes').addEventListener("keyup", updateValues);
 
 function updateValues() {
-  document.querySelector('.montant-tva').value = parseInt((parseInt(document.querySelector('.montant-hors-taxes').value) / 100) * parseInt(document.getElementById('input-tva').value));
-  document.querySelector('.montant-ttc').value = (parseInt(document.querySelector('.montant-hors-taxes').value)) + parseInt(document.querySelector('.montant-tva').value);
+  document.querySelector('.montant-tva').value = (parseInt(document.querySelector('.montant-hors-taxes').value)) / 100 * parseFloat(document.getElementById('input-tva').value);
+  document.querySelector('.montant-ttc').value = (parseInt(document.querySelector('.montant-hors-taxes').value)) + parseFloat(document.querySelector('.montant-tva').value);
   document.querySelector('.bottom-hors-taxes').textContent = document.querySelector('.montant-hors-taxes').value + "$";
   document.querySelector('.bottom-tva').textContent = document.querySelector('.montant-tva').value + "$";
   document.querySelector('.bottom-ttc').textContent = document.querySelector('.montant-ttc').value + "$";
