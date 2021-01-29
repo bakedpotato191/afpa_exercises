38654c588224d67d38fe2b90f030f6ae2556a337
bakedpotato191 <vladislavp@pm.me>
2021-01-29 12:44:45 +0100

    ...

--- 29-01/script.js
+++ 29-01/script.js
@@ -3,6 +3,7 @@ document.querySelectorAll('.tva').forEach(item => {
     if (document.querySelector('.montant-hors-taxes').value == "") {
       document.getElementById('input-tva').value = item.textContent;
     } else {
+      document.getElementById('input-tva').value = item.textContent;
       console.log(parseInt(document.querySelector('.montant-hors-taxes').value));
       document.querySelector('.montant-tva').value = (parseInt(document.querySelector('.montant-hors-taxes').value)) / 100 * parseFloat(item.textContent);
       document.querySelector('.montant-ttc').value = (parseInt(document.querySelector('.montant-hors-taxes').value)) + parseFloat(document.querySelector('.montant-tva').value);
@@ -13,11 +14,12 @@ document.querySelectorAll('.tva').forEach(item => {
   })
 });
 
-document.querySelector('.montant-hors-taxes').addEventListener("keyup", event => {
+document.querySelector('.montant-hors-taxes').addEventListener("keyup", updateValues);
+
+function updateValues() {
   document.querySelector('.montant-tva').value = parseInt((parseInt(document.querySelector('.montant-hors-taxes').value) / 100) * parseInt(document.getElementById('input-tva').value));
   document.querySelector('.montant-ttc').value = (parseInt(document.querySelector('.montant-hors-taxes').value)) + parseInt(document.querySelector('.montant-tva').value);
   document.querySelector('.bottom-hors-taxes').textContent = document.querySelector('.montant-hors-taxes').value + "$";
   document.querySelector('.bottom-tva').textContent = document.querySelector('.montant-tva').value + "$";
   document.querySelector('.bottom-ttc').textContent = document.querySelector('.montant-ttc').value + "$";
-
-});
+}
