903a3e2b31ba533fbce0c1f0a28fcb4519fbad82
bakedpotato191 <vladislavp@pm.me>
2021-01-29 12:07:36 +0100

    finaliser

--- 29-01/script.js
+++ 29-01/script.js
@@ -1,7 +1,15 @@
 document.querySelectorAll('.tva').forEach(item => {
   item.addEventListener('click', event => {
-
-    document.getElementById('input-tva').value = item.textContent;
+    if (document.querySelector('.montant-hors-taxes').value == "") {
+      document.getElementById('input-tva').value = item.textContent;
+    } else {
+      console.log(parseInt(document.querySelector('.montant-hors-taxes').value));
+      document.querySelector('.montant-tva').value = (parseInt(document.querySelector('.montant-hors-taxes').value)) / 100 * parseFloat(item.textContent);
+      document.querySelector('.montant-ttc').value = (parseInt(document.querySelector('.montant-hors-taxes').value)) + parseFloat(document.querySelector('.montant-tva').value);
+      document.querySelector('.bottom-hors-taxes').textContent = document.querySelector('.montant-hors-taxes').value + "$";
+      document.querySelector('.bottom-tva').textContent = document.querySelector('.montant-tva').value + "$";
+      document.querySelector('.bottom-ttc').textContent = document.querySelector('.montant-ttc').value + "$";
+    }
   })
 });
 
