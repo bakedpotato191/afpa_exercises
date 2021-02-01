195764feaf8f18961c037b3b11b96b9f8a40bed2
bakedpotato191 <vladislavp@Pm.me>
2021-02-01 16:04:08 +0100

    01-02-2021

--- Janvier/28-01/script.js
+++ Janvier/28-01/script.js
@@ -1,22 +1,17 @@
 var i, j, index, thlen;
 document.querySelector('.addrow').addEventListener("click", event => {
-  //create new row and append it to the tbody selector
   var nouvel_tr = document.createElement('tr');
   nouvel_tr.className = 'etudiants';
   document.querySelector('tbody').append(nouvel_tr);
-  //get quantity of columns in thead
   thlen = document.querySelectorAll('[scope="col"]').length;
   for (index = 0; index < thlen; index++) {
-    //append new th to the first column in the row
     if (index == 0) {
       var nouvel_th = document.createElement('th');
       nouvel_th.scope = 'row';
       row_nombre = document.querySelectorAll('.etudiants').length;
       nouvel_th.innerHTML = row_nombre;
       nouvel_tr.append(nouvel_th);
-    }
-    //if not first column add input forms to every cell
-    else {
+    } else {
       var contenu_tr = document.createElement('td');
       var contenu_td = document.createElement('input')
       contenu_tr.appendChild(contenu_td);
--- Janvier/29-01/script.js
+++ Janvier/29-01/script.js
@@ -10,9 +10,19 @@ document.querySelectorAll('.tva').forEach(item => {
 });
 
 document.querySelector('.montant-hors-taxes').addEventListener("keyup", updateValues);
+document.querySelector('#input-tva').addEventListener('keyup', event => {
+  event = (event) ? event : window.event;
+  var charCode = (event.which) ? event.which : event.keyCode;
+  if ((charCode > 31 && charCode !== 37) && (charCode < 48 || charCode > 57)) {
+    event.preventDefault();
+    return false;
+  }
+
+  updateValues();
+});
 
 function updateValues() {
-  document.querySelector('.montant-tva').value = (parseInt(document.querySelector('.montant-hors-taxes').value)) / 100 * parseFloat(document.getElementById('input-tva').value);
+  document.querySelector('.montant-tva').value = (Math.floor((parseInt(document.querySelector('.montant-hors-taxes').value)) / 100 * parseFloat(document.getElementById('input-tva').value) * 100) / 100).toFixed(2);
   document.querySelector('.montant-ttc').value = (parseInt(document.querySelector('.montant-hors-taxes').value)) + parseFloat(document.querySelector('.montant-tva').value);
   document.querySelector('.bottom-hors-taxes').textContent = document.querySelector('.montant-hors-taxes').value + "$";
   document.querySelector('.bottom-tva').textContent = document.querySelector('.montant-tva').value + "$";
