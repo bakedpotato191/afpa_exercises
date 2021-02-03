935c2f7d3be43a40a03843c136d4c4a668fd1111
bakedpotato191 <vladislavp@pm.me>
2021-02-03 16:09:56 +0100

    Exercise mots et lettres

---
+++ Fevrier/03-02/mots-lettres/style.js
@@ -0,0 +1,19 @@
+$('#exampleFormControlTextarea1').on("input", function(e) {
+  if (event.data != " ") {
+    var mots_array = $(this).val().split(" ");
+    var mots = 0;
+    var lettres = 0;
+    for (var index = 0; index < mots_array.length; index++) {
+      if (mots_array[index] !== "") {
+        mots++;
+        lettres = lettres + mots_array[index].length;
+      }
+    }
+    if ($('.mots').length == 0) {
+      $('form').append("<label class='mots'></label>")
+      $('.mots').html(mots + " mots" + " / " + lettres + " lettres");
+    } else {
+      $('.mots').html(mots + " mots" + " / " + lettres + " lettres");
+    }
+  }
+});
