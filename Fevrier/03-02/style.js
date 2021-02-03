1c52f92fb9513b3c98515d4089ea20bb0dd0a010
bakedpotato191 <vladislavp@pm.me>
2021-02-03 15:03:34 +0100

    Corrections finales

--- Fevrier/03-02/style.js
+++ Fevrier/03-02/style.js
@@ -13,7 +13,7 @@ $("input[name='poids']").keypress(function() {
 });
 
 $("input[type='submit']").click(function() {
-  if ($("input[name='taille']").val() !== "" && $("input[name='poids']").val() !== "") {
+  if ($("input[name='taille']").val() !== "" && $("input[name='taille']").val() > 0 && $("input[name='poids']").val() !== "") {
     var taille = $("input[name='taille']").val();
     var poids = $("input[name='poids']").val();
 
@@ -26,24 +26,24 @@ $("input[type='submit']").click(function() {
       $('.resultat').append("<span class='text2'>Votre IMC (indice de masse corporelle) est de:</span>");
       $('.resultat').append("<span class='nombre'>" + imc + "</span>");
       if (imc < 18.5) {
-        $('.resultat').append("<span class='type'> IMC < 18.5: insuffisance pondérale </span>");
+        $('.resultat').append("<span class='type'> IMC < 18.5 : insuffisance pondérale </span>");
       } else if (imc >= 18.5 && imc <= 25) {
-        $('.resultat').append("<span class='type'> 18,5 < IMC < 24,9: poids normal </span>");
+        $('.resultat').append("<span class='type'> 18,5 < IMC < 24,9 : poids normal </span>");
       } else if (imc > 25 && imc <= 30) {
-        $('.resultat').append("<span class='type'> 25 < IMC < 29,9: surpoids </span>");
+        $('.resultat').append("<span class='type'> 25 < IMC < 29,9 : surpoids </span>");
       } else {
-        $('.resultat').append("<span class='type'> IMC > 30: obésité </span>");
+        $('.resultat').append("<span class='type'> IMC > 30 : obésité </span>");
       }
     } else {
       $('.nombre').html(imc)
       if (imc < 18.5) {
-        $('.type').html('IMC < 18.5: insuffisance pondérale');
+        $('.type').html('IMC < 18.5 : insuffisance pondérale');
       } else if (imc >= 18.5 && imc <= 25) {
-        $('.type').html('18,5 < IMC < 24,9: poids normal');
+        $('.type').html('18,5 < IMC < 24,9 : poids normal');
       } else if (imc > 25 && imc <= 30) {
-        $('.type').html('25 < IMC < 29,9: surpoids');
+        $('.type').html('25 < IMC < 29,9 : surpoids');
       } else {
-        $('.type').html('IMC > 30:obésité');
+        $('.type').html('IMC > 30 : obésité');
       }
     }
   } else {
