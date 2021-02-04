9f120d9fe63ebe290fc501f4ad0b228ed6b2114e
bakedpotato191 <vladislavp@pm.me>
2021-02-04 22:04:18 +0100

    Utiliser jquery pour appliquer les propriétés css

--- Fevrier/04-02/form-contact/style.js
+++ Fevrier/04-02/form-contact/style.js
@@ -1,110 +1,144 @@
-  var limit = 200;
-
-  $('#inputNom, #inputPrenom').on("keypress", function() {
-    if (regexNomPrenom(event.key)) {
-      return true;
-    } else {
-      event.preventDefault();
-      return false;
-    }
+$(function() {
+  $('textarea').css({
+    "resize": "none"
   });
-
-  $('#inputNom, #inputPrenom').on("input", function() {
-    if ($(this).val().length > 0) {
-      setCorrect($(this));
-    } else {
-      setPasCorrect($(this));
-    }
+  $('form').css({
+    "margin-top": "50px"
   });
+});
 
-  $('#inputEmail4').on("input", function() {
-    var regex = /^\w+([-+.'][^\s]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
-    if (regex.test($(this).val())) {
-      setCorrect($(this));
-    } else {
-      setPasCorrect($(this));
-    }
-  });
+var limit = 200;
 
-  $('#inputTel').on("keypress", function() {
-    if (regexTelephone(event.key)) {
-      return true;
-    } else {
-      event.preventDefault();
-      return false;
-    }
-  });
+$('#inputNom, #inputPrenom').on("keypress", function() {
+  if (regexNomPrenom(event.key)) {
+    return true;
+  } else {
+    event.preventDefault();
+    return false;
+  }
+});
 
-  $('#inputTel').on("input", function() {
-    var phoneExp = /^((\+)33|0|0033)[1-9](\d{2}){4}$/g;
-    if (phoneExp.test($(this).val())) {
-      setCorrect($(this));
-    } else {
-      setPasCorrect($(this));
-    }
-  });
+$('#inputNom, #inputPrenom').on("input", function() {
+  if ($(this).val().length > 0) {
+    setCorrect($(this));
+  } else {
+    setPasCorrect($(this));
+  }
+});
 
-  $('#exampleFormControlTextarea1').on("input", function() {
-    var mots_array = $(this).val().split(" ");
-    var spaceCount = ($(this).val().split(" ").length - 1);
-    var mots = 0;
-    var caracteres = 0;
-    for (var index = 0; index < mots_array.length; index++) {
-      if (mots_array[index] !== "") {
-        mots++;
-        caracteres = caracteres + mots_array[index].length;
-      }
-    }
-    if ($(this).val().length > 0) {
-      setCorrect($(this));
-    } else {
-      setPasCorrect($(this));
-    }
+$('#inputEmail4').on("input", function() {
+  var regex = /^\w+([-+.'][^\s]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+  if (regex.test($(this).val())) {
+    setCorrect($(this));
+  } else {
+    setPasCorrect($(this));
+  }
+});
 
-    $('.mots').html(mots + " Mot(s)" + " / " + (limit - caracteres) + " Caractère(s)" + " / 200");
-    $('#exampleFormControlTextarea1').attr("maxlength", limit + spaceCount);
+$('#inputTel').on("keypress", function() {
+  if (regexTelephone(event.key)) {
+    return true;
+  } else {
+    event.preventDefault();
+    return false;
+  }
+});
 
-  });
+$('#inputTel').on("input", function() {
+  var phoneExp = /^((\+)33|0|0033)[1-9](\d{2}){4}$/g;
+  if (phoneExp.test($(this).val())) {
+    setCorrect($(this));
+  } else {
+    setPasCorrect($(this));
+  }
+});
 
-  $("button").on("click", function() {
-    if ($('input').not('.info').length == 0 && $('textarea').hasClass('info')) {
-      var prenom = $('#inputPrenom').val();
-      $('form').css("display", "none");
-      $('.container').append("<div class='success'> Merci " + prenom + ", votre message a été bien envoyé!")
-    } else {
-      if ($('.error').length > 0) {
-        return;
-      } else {
-        $('form').append("<div> Remplir tous les champs </div>")
-        $('form').children().last().addClass('error');
-      }
-    }
-  });
-
-  function regexNomPrenom(key) {
-    var regex = /^[a-zA-ZÀ-ÿ-. ]*$/;
-    if (regex.test(key)) {
-      return true;
-    } else {
-      return false;
+$('#exampleFormControlTextarea1').on("input", function() {
+  var mots_array = $(this).val().split(" ");
+  console.log(mots_array.length);
+  var spaceCount = ($(this).val().split(" ").length - 1);
+  var mots = 0;
+  var caracteres = 0;
+  for (var index = 0; index < mots_array.length; index++) {
+    if (mots_array[index] !== "") {
+      mots++;
+      caracteres = caracteres + mots_array[index].length;
     }
   }
+  if ($(this).val().length > 0) {
+    setCorrect($(this));
+  } else {
+    setPasCorrect($(this));
+  }
 
-  function regexTelephone(key) {
-    var regex = /^[0-9+]*$/;
-    if (regex.test(key)) {
-      return true;
+  $('.mots').html(mots + " Mot(s)" + " / " + (limit - caracteres) + " Caractère(s)" + " / 200");
+  $('#exampleFormControlTextarea1').attr("maxlength", limit + spaceCount);
+
+});
+
+$("button").on("click", function() {
+  if ($('input').not('.info').length == 0 && $('textarea').hasClass('info')) {
+    var prenom = $('#inputPrenom').val();
+    $('form').hide();
+    $('.container').append("<div class='success'> Merci " + prenom + ", votre message a été bien envoyé!")
+    $('.success').css({
+      "color": "green",
+      "font-size": "18px",
+      "font-weight": "700",
+      "display": "flex",
+      "flex-direction": "column",
+      "justify-content": "center",
+      "align-items": "center",
+      "text-align": "center",
+      "min-height": "100vh"
+    });
+  } else {
+    if ($('.error').length > 0) {
+      return;
     } else {
-      return false;
+      $('form').append("<div> Remplir tous les champs </div>")
+      $('form').children().last().addClass('error');
+      $('.error').css({
+        "color": "red",
+        "font-size": "16px",
+        "font-weight": "bold",
+        "font-family": "sans-serif",
+        "margin-top": "10px"
+      });
     }
   }
+});
 
-  function setCorrect(element) {
-    element.removeClass('vide');
-    element.addClass('info');
+function regexNomPrenom(key) {
+  var regex = /^[a-zA-ZÀ-ÿ-. ]*$/;
+  if (regex.test(key)) {
+    return true;
+  } else {
+    return false;
   }
+}
 
-  function setPasCorrect(element) {
-    element.removeClass('info');
-    element.addClass('vide');
+function regexTelephone(key) {
+  var regex = /^[0-9+]*$/;
+  if (regex.test(key)) {
+    return true;
+  } else {
+    return false;
   }
+}
+
+function setCorrect(element) {
+  element.removeClass('vide');
+  element.addClass('info');
+  $('.info').css({
+    "border": "2px solid green"
+  });
+}
+
+function setPasCorrect(element) {
+  element.removeClass('info');
+  element.addClass('vide');
+  $('.vide').css({
+    "border": "2px solid red"
+  });
+}
