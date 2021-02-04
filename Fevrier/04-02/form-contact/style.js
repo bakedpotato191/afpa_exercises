765350ea476133eb283e70cc04421640bd1cc9f2
bakedpotato191 <vladislavp@pm.me>
2021-02-04 15:15:39 +0100

    Fix formatting

--- Fevrier/04-02/form-contact/style.js
+++ Fevrier/04-02/form-contact/style.js
@@ -1,27 +1,6 @@
 $('document').ready(function() {
+
   var limit = 200;
-  $('#exampleFormControlTextarea1').on("input", function() {
-    var mots_array = $(this).val().split(" ");
-    var spaceCount = ($(this).val().split(" ").length - 1);
-    var mots = 0;
-    var caracteres = 0;
-    for (var index = 0; index < mots_array.length; index++) {
-      if (mots_array[index] !== "") {
-        mots++;
-        caracteres = caracteres + mots_array[index].length;
-      }
-    }
-    if ($(this).val().length > 0) {
-      setCorrect($(this));
-    } else {
-      setPasCorrect($(this));
-    }
-
-    $('.mots').html(mots + " Mot(s)" + " / " + (limit - caracteres) + " Caractère(s)" + " / 200");
-    $('#exampleFormControlTextarea1').attr("maxlength", limit + spaceCount);
-
-  });
-
 
   $('#inputNom, #inputPrenom').on("keypress", function() {
     if (regexNomPrenom(event.key)) {
@@ -39,24 +18,6 @@ $('document').ready(function() {
       setPasCorrect($(this));
     }
   });
-
-  function regexNomPrenom(key) {
-    var regex = /^[a-zA-ZÀ-ÿ-. ]*$/;
-    if (regex.test(key)) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  function regexTelephone(key) {
-    var regex = /^[0-9+]*$/;
-    if (regex.test(key)) {
-      return true;
-    } else {
-      return false;
-    }
-  }
 
   $('#inputEmail4').on("input", function() {
     var regex = /^\w+([-+.'][^\s]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
@@ -85,6 +46,28 @@ $('document').ready(function() {
     }
   });
 
+  $('#exampleFormControlTextarea1').on("input", function() {
+    var mots_array = $(this).val().split(" ");
+    var spaceCount = ($(this).val().split(" ").length - 1);
+    var mots = 0;
+    var caracteres = 0;
+    for (var index = 0; index < mots_array.length; index++) {
+      if (mots_array[index] !== "") {
+        mots++;
+        caracteres = caracteres + mots_array[index].length;
+      }
+    }
+    if ($(this).val().length > 0) {
+      setCorrect($(this));
+    } else {
+      setPasCorrect($(this));
+    }
+
+    $('.mots').html(mots + " Mot(s)" + " / " + (limit - caracteres) + " Caractère(s)" + " / 200");
+    $('#exampleFormControlTextarea1').attr("maxlength", limit + spaceCount);
+
+  });
+
   $("button").on("click", function() {
     if ($('input').not('.info').length == 0 && $('textarea').hasClass('info')) {
       var prenom = $('#inputPrenom').val();
@@ -99,6 +82,24 @@ $('document').ready(function() {
       }
     }
   });
+
+  function regexNomPrenom(key) {
+    var regex = /^[a-zA-ZÀ-ÿ-. ]*$/;
+    if (regex.test(key)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function regexTelephone(key) {
+    var regex = /^[0-9+]*$/;
+    if (regex.test(key)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
   function setCorrect(element) {
     element.removeClass('vide');
