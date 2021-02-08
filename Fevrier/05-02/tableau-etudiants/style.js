3b6455b9951559d28f17c16ca861ad2b8443c8ea
bakedpotato191 <vladislavp@Pm.me>
2021-02-08 15:58:09 +0100

    Fin de journee

--- Fevrier/05-02/tableau-etudiants/style.js
+++ Fevrier/05-02/tableau-etudiants/style.js
@@ -1,4 +1,4 @@
-var inputs, editButton, deleteButton, editNumber, i;
+var inputs, editButton, editNumber, i;
 
 $(function() {
   $('.btn-info').prop("disabled", true);
@@ -18,71 +18,8 @@ $(function() {
 
 $(".btn-primary").on("click", function() {
   if (verifierChamps()) {
-    $('<tr></tr>').appendTo('tbody');
-    $('<th></th>').attr("scope", "row").css({
-      "text-align": "center",
-      "vertical-align": "middle"
-    }).html($('tbody').children('tr').length).appendTo('tr:last');
-
-    var classes = ['prenom', 'nom', 'email', 'telephone'];
-    for (var i = 0; i < classes.length; i++) {
-      $('<td></td>').attr("class", classes[i]).css({
-        "text-align": "center",
-        "vertical-align": "middle"
-      }).html(inputs.eq(i).val()).appendTo('tr:last');
-    }
-
-    $('<td></td>').css({
-      "display": "flex",
-      "justify-content": "center",
-    }).appendTo('tr:last');
-
-    editNumber = $('.btn-modifier').length;
-
-    $('<button></button>').attr({
-      "type": "button",
-      "class": "btn btn-success btn-modifier",
-      "value": editNumber + 1
-    }).css({
-      "margin-left": "10px"
-    }).html('<i class="fa fa-pencil-square-o" aria-hidden="true"></i>').appendTo('td:last');
-
-    deleteButton = $('.btn-supprimer').length;
-
-    $('<button></button>').attr({
-      "type": "button",
-      "class": "btn btn-danger btn-supprimer",
-      "value": deleteButton + 1
-    }).css("margin-left", "10px").html('<i class="fa fa-trash" aria-hidden="true"></i>').appendTo('td:last');
-
-    $('.btn-primary').blur();
-    $('.btn-modifier').on("click", function() {
-      var child = $(this).parent().parent().children();
-      for (i = 1; i < child.length - 1; i++) {
-        inputs.eq(i - 1).val(child[i].textContent);
-      }
-      $('.btn-primary').prop("disabled", true);
-      $('.btn-info').prop("disabled", false);
-      editButton = $(this).val();
-      for (i = 0; i < inputs.length; i++) {
-        setCorrect(inputs.eq(i));
-      }
-    });
-
-    $(".btn-supprimer").unbind('click').click(function() {
-      if (confirm("Supprimer l'étudiant?")) {
-        $(this).closest('tr').remove();
-        if (!($('tbody').children('tr').length)) {
-          $('.btn-info').prop("disabled", true);
-          $('.btn-primary').prop("disabled", false);
-        }
-        var element_th = $('tbody').find('th');
-        for (var i = 0; i < element_th.length; i++) {
-          element_th.eq(i).html(i + 1);
-        }
-      }
-    });
-    $('button:last').html('<i class="fa fa-trash" aria-hidden="true"></i>');
+    creationLigne();
+    ajouterLesBoutons();
     resetInput();
   }
 });
@@ -144,6 +81,73 @@ $('#inputNom, #inputPrenom,#inputEmail4, #inputTel').on('keyup', function(e) {
   }
 });
 
+function creationLigne() {
+  $('<tr></tr>').appendTo('tbody');
+  $('<th></th>').attr("scope", "row").css({
+    "text-align": "center",
+    "vertical-align": "middle"
+  }).html($('tbody').children('tr').length).appendTo('tr:last');
+
+  var classes = ['prenom', 'nom', 'email', 'telephone'];
+  for (var i = 0; i < classes.length; i++) {
+    $('<td></td>').attr("class", classes[i]).css({
+      "text-align": "center",
+      "vertical-align": "middle"
+    }).html(inputs.eq(i).val()).appendTo('tr:last');
+  }
+
+  $('<td></td>').css({
+    "display": "flex",
+    "justify-content": "center",
+  }).appendTo('tr:last');
+
+  editNumber = $('.btn-modifier').length;
+
+  $('<button></button>').attr({
+    "type": "button",
+    "class": "btn btn-success btn-modifier",
+    "value": editNumber + 1
+  }).css({
+    "margin-left": "10px"
+  }).html('<i class="fa fa-pencil-square-o" aria-hidden="true"></i>').appendTo('td:last');
+
+  $('<button></button>').attr({
+    "type": "button",
+    "class": "btn btn-danger btn-supprimer"
+  }).css("margin-left", "10px").html('<i class="fa fa-trash" aria-hidden="true"></i>').appendTo('td:last');
+}
+
+function ajouterLesBoutons() {
+  $('.btn-primary').blur();
+  $('.btn-modifier').on("click", function() {
+    var child = $(this).parent().parent().children();
+    for (i = 1; i < child.length - 1; i++) {
+      inputs.eq(i - 1).val(child[i].textContent);
+    }
+    $('.btn-primary').prop("disabled", true);
+    $('.btn-info').prop("disabled", false);
+    editButton = $(this).val();
+    for (i = 0; i < inputs.length; i++) {
+      setCorrect(inputs.eq(i));
+    }
+  });
+
+  $(".btn-supprimer").unbind('click').click(function() {
+    if (confirm("Supprimer l'étudiant?")) {
+      $(this).closest('tr').remove();
+      if (!($('tbody').children('tr').length)) {
+        $('.btn-info').prop("disabled", true);
+        $('.btn-primary').prop("disabled", false);
+      }
+      var element_th = $('tbody').find('th');
+      for (var i = 0; i < element_th.length; i++) {
+        element_th.eq(i).html(i + 1);
+      }
+    }
+  });
+  $('button:last').html('<i class="fa fa-trash" aria-hidden="true"></i>');
+}
+
 function regexNomPrenom(key) {
   var regex = /^[a-zA-ZÀ-ÿ-. ]*$/;
   if (regex.test(key)) {
@@ -162,14 +166,6 @@ function regexTelephone(key) {
   }
 }
 
-function setCorrect(element) {
-  element.removeClass('vide');
-  element.addClass('info');
-  $('.info').css({
-    "border": "2px solid green"
-  });
-}
-
 function verifierChamps() {
   var count = 0;
   for (i = 0; i < inputs.length; i++) {
@@ -183,6 +179,14 @@ function verifierChamps() {
   } else {
     return true;
   }
+}
+
+function setCorrect(element) {
+  element.removeClass('vide');
+  element.addClass('info');
+  $('.info').css({
+    "border": "2px solid green"
+  });
 }
 
 function setPasCorrect(element) {
