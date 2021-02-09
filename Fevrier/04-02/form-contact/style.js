32d061966e35ccffcd651ed767c5df4b1762cb7e
bakedpotato191 <vladislavp@pm.me>
2021-02-09 22:26:29 +0100

    Mettre a jour ancien exercice

--- Fevrier/04-02/form-contact/style.js
+++ Fevrier/04-02/form-contact/style.js
@@ -10,16 +10,11 @@ $(function() {
 var limit = 200;
 
 $('#inputNom, #inputPrenom').on("keypress", function() {
-  if (regexNomPrenom(event.key)) {
-    return true;
-  } else {
-    event.preventDefault();
-    return false;
-  }
+  return(regexNomPrenom(event.key));
 });
 
 $('#inputNom, #inputPrenom').on("input", function() {
-  if ($(this).val().length > 0) {
+  if ($(this).val().length) {
     setCorrect($(this));
   } else {
     setPasCorrect($(this));
@@ -28,7 +23,7 @@ $('#inputNom, #inputPrenom').on("input", function() {
 
 $('#inputEmail4').on("input", function() {
   var regex = /^\w+([-+.'][^\s]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
-  if (regex.test($(this).val())) {
+  if (regexEmail($(this).val())) {
     setCorrect($(this));
   } else {
     setPasCorrect($(this));
@@ -36,12 +31,8 @@ $('#inputEmail4').on("input", function() {
 });
 
 $('#inputTel').on("keypress", function() {
-  if (regexTelephone(event.key)) {
-    return true;
-  } else {
-    event.preventDefault();
-    return false;
-  }
+  return (regexTelephone(event.key));
+
 });
 
 $('#inputTel').on("input", function() {
@@ -55,7 +46,6 @@ $('#inputTel').on("input", function() {
 
 $('#exampleFormControlTextarea1').on("input", function() {
   var mots_array = $(this).val().split(" ");
-  console.log(mots_array.length);
   var spaceCount = ($(this).val().split(" ").length - 1);
   var mots = 0;
   var caracteres = 0;
@@ -80,8 +70,7 @@ $("button").on("click", function() {
   if ($('input').not('.info').length == 0 && $('textarea').hasClass('info')) {
     var prenom = $('#inputPrenom').val();
     $('form').hide();
-    $('.container').append("<div class='success'> Merci " + prenom + ", votre message a été bien envoyé!")
-    $('.success').css({
+    $('<div></div>').html("Merci " + prenom + ", votre message a été bien envoyé!").addClass('.success').css({
       "color": "green",
       "font-size": "18px",
       "font-weight": "700",
@@ -91,54 +80,47 @@ $("button").on("click", function() {
       "align-items": "center",
       "text-align": "center",
       "min-height": "100vh"
-    });
+    }).appendTo('.container');
   } else {
-    if ($('.error').length > 0) {
+    if ($('.error').length) {
       return;
     } else {
-      $('form').append("<div> Remplir tous les champs </div>")
-      $('form').children().last().addClass('error');
-      $('.error').css({
+      $('<div></div>').html('Remplir tous les champs').addClass('error').css({
         "color": "red",
         "font-size": "16px",
         "font-weight": "bold",
         "font-family": "sans-serif",
         "margin-top": "10px"
-      });
+      }).appendTo('form');
     }
   }
 });
 
 function regexNomPrenom(key) {
   var regex = /^[a-zA-ZÀ-ÿ-. ]*$/;
-  if (regex.test(key)) {
-    return true;
-  } else {
-    return false;
-  }
+  return (regex.test(key));
 }
 
 function regexTelephone(key) {
   var regex = /^[0-9+]*$/;
-  if (regex.test(key)) {
-    return true;
-  } else {
-    return false;
-  }
+  return(regex.test(key));
+}
+
+function regexEmail(value){
+  var regex = /^\w+([-+.'][^\s]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+  return(regex.test(value));
 }
 
 function setCorrect(element) {
   element.removeClass('vide');
-  element.addClass('info');
-  $('.info').css({
+  element.addClass('info').css({
     "border": "2px solid green"
   });
 }
 
 function setPasCorrect(element) {
   element.removeClass('info');
-  element.addClass('vide');
-  $('.vide').css({
+  element.addClass('vide').css({
     "border": "2px solid red"
   });
 }
