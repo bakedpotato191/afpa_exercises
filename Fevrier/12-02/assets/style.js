b8eab61b73d1866593d60e0017ef9833db7a2427
bakedpotato191 <vladislavp@Pm.me>
2021-02-15 16:00:42 +0100

    envoyer dernien modifications

--- Fevrier/12-02/assets/style.js
+++ Fevrier/12-02/assets/style.js
@@ -1,34 +1,21 @@
 $(document).ready(function() {
-
+  var item;
   $.ajax({
     url: "assets/items.json",
     dataType: "json",
     success: function(data) {
 
-      var item = "<li class='list-group-item'>";
-      item += "<form> <div class=form-group row item-name>";
-      item += "<p style='font-weight: bold; padding-bottom:40px'> </p></div>";
-      item += "<div class='form-group row item-row' style='margin-left: 0px; margin-bottom:0px'>";
-      item += "<label for='quantite'>Quantité:</label>";
-      item += "<div class='col col-sm-3'>";
-      item += "<input type='text' class='form-control form-control-sm quantite' placeholder='' autocomplete='off'> </div>";
-      item += "<label for='prix'>Prix:</label>";
-      item += "<div class='col col-sm-3'>";
-      item += "<input type='text' class='form-control form-control-sm prix' placeholder='' readonly> </div>";
-      item += "<label for='total'>Total:</label>";
-      item += "<div class='col col-sm-3'>";
-      item += "<input type='text' class='form-control form-control-sm total' placeholder='' readonly autocomplete='off'>";
-      item += "</div> </div> </form> </li>";
+      ajouterProduit();
 
       for (var i = 0; i < data.items.length; i++) {
+        $('.dropdown-menu li a').eq(i).text(data.items[i].nom);
         $('.list-group').append(item);
-        $('p').eq(i).html(data.items[i].nom)
+        $('form p').eq(i).html(data.items[i].nom + "<hr>")
         $('input.prix').eq(i).val(data.items[i].prix);
-
         if (i % 2 === 0) {
-          $('li').eq(i).addClass("list-group-item-success");
+          $('ul:last li').eq(i).addClass("list-group-item-success");
         } else {
-          $('li').eq(i).addClass("list-group-item-primary");
+          $('ul:last li').eq(i).addClass("list-group-item-primary");
         }
       }
 
@@ -44,7 +31,28 @@ $(document).ready(function() {
       });
       $("label[for='total']").css("font-weight", "bold");
       $('input.net').css("font-weight", "bold");
-      $('.container').css("background-color", "#f3f3f3");
+      $('.container').css({
+        "background-color": "#f3f3f3",
+        "padding-top": "20px "
+      });
+
+      function ajouterProduit() {
+        item = "<li class='list-group-item'>";
+        item += "<form> <div class=form-group row item-name>";
+        item += "<p style='font-weight: bold; padding-bottom:40px'> </p></div>";
+        item += "<div class='form-group row item-row' style='margin-left: 0px; margin-bottom:0px'>";
+        item += "<label for='quantite'>Quantité:</label>";
+        item += "<div class='col col-sm-3'>";
+        item += "<input type='text' class='form-control form-control-sm quantite' placeholder='' autocomplete='off'> </div>";
+        item += "<label for='prix'>Prix:</label>";
+        item += "<div class='col col-sm-3'>";
+        item += "<input type='text' class='form-control form-control-sm prix' placeholder='' readonly> </div>";
+        item += "<label for='total'>Total:</label>";
+        item += "<div class='col col-sm-3'>";
+        item += "<input type='text' class='form-control form-control-sm total' placeholder='' readonly autocomplete='off'></div>";
+        item += "<div class='col'> <button type='button' class='btn btn-danger retirer'>Retirer</button></div>"
+        item += "</div> </form> </li>";
+      }
 
     },
     error: function(status, text) {
@@ -59,6 +67,18 @@ $(document).ready(function() {
 
     $('input.quantite').keypress(function() {
       return (regexQuantite(event.key))
+    });
+
+    $('.retirer').on("click", function() {
+      if (confirm("Voulez-vous vraiment retirer l'article?")) {
+        $(this).closest('li').remove();
+      }
+    });
+
+    $('.dropdown-menu li a').on("click", function() {
+      $('button.dropdown-toggle').text($(this).text());
+
+
     });
 
     function setTotal() {
