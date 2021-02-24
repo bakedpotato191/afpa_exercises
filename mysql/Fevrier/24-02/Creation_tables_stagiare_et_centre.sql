ef70facfaea26c230df7c90b9c832e2b27eab3f9
bakedpotato191 <vladislavp@pm.me>
2021-02-24 11:51:30 +0100

    encore simplifier le script

--- mysql/Fevrier/24-02/Creation_tables_stagiare_et_centre.sql
+++ mysql/Fevrier/24-02/Creation_tables_stagiare_et_centre.sql
@@ -1,22 +1,19 @@
 use formation;
 
-CREATE TABLE `formation`.`stagiaire` (
-	`Id` INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    `Nom` VARCHAR(30) NOT NULL,
-    `Prénom` VARCHAR(30) NOT NULL,
-    `Email` VARCHAR(30) NOT NULL
-								);
+CREATE TABLE stagiaire (id int unsigned auto_increment primary key, nom varchar(50), prenom varchar(50), email varchar(50));
 
-insert into stagiaire (Nom, Prénom, Email) values ('Chirac', 'Jacques', 'jacques@gmail.com'), ('Dupont', 'Jean', 'jeandupont@gmail.com'), ('Henri', 'Zack', 'zackhenri@gmail.com'), ('Bidule', 'Machin', 'mbidule@gmail.com');
+insert into stagiaire (nom, prenom, email) values 
+	('Chirac', 'Jacques', 'jacques@gmail.com'), 
+	('Dupont', 'Jean', 'jeandupont@gmail.com'), 
+	('Henri', 'Zack', 'zackhenri@gmail.com'), 
+	('Bidule', 'Machin', 'mbidule@gmail.com');
 
-CREATE TABLE `formation`.`centre` (
-	`Id` INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    `Nom` VARCHAR(30) NOT NULL,
-    `Lieu` VARCHAR(30) NOT NULL,
-    `Specialité` VARCHAR(30) NOT NULL
-								);
+CREATE TABLE centre (id int unsigned auto_increment primary key, nom varchar(10), lieu varchar(20), specialite varchar(10));
                                 
-INSERT into centre (Nom, Lieu, Specialité) values ('Afpa', 'Paris', 'CDA'), ('Afpa', 'Marseille', 'Dev Mobile'), ('Afpa', 'Lyon', 'Marketing');
+INSERT into centre (nom, lieu, specialite) values 
+	('Afpa', 'Paris', 'CDA'), 
+	('Afpa', 'Marseille', 'Dev Mobile'), 
+	('Afpa', 'Lyon', 'Marketing');
 
 
 
