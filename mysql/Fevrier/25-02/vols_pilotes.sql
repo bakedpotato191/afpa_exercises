c8257608dd75eb3ae626fb5be66a25d043381245
bakedpotato191 <vladislavp@pm.me>
2021-02-25 13:16:34 +0100

    Mettre a jour

--- mysql/Fevrier/25-02/vols_pilotes.sql
+++ mysql/Fevrier/25-02/vols_pilotes.sql
@@ -1,36 +1,97 @@
 use formation;
 
+/* table avions */
+CREATE TABLE avions (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    NumAv VARCHAR(5) NOT NULL,
+    NomAv varchar(5) NOT NULL,
+    Capacité int(3) NOT NULL,
+    Localisation varchar(10) NOT NULL
+);
+
+INSERT into avions (NumAv, NomAv, Capacité, Localisation) values 
+('00001', 'Alpha', '300', 'Paris'), 
+('00002', 'Delta', '400', 'Milan'), 
+('00003', 'Delta', '350', 'Rome');
+
+
+/* table vols */
 CREATE TABLE vols (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    numero_vols VARCHAR(6) NOT NULL,
-    code_vol INT(6) NOT NULL,
-    id_pilote VARCHAR(5) NOT NULL,
-    ville_depart VARCHAR(15) NOT NULL,
-    ville_arrive VARCHAR(15) NOT NULL,
-    temp_depart TIME NOT NULL,
-    temp_arrive TIME NOT NULL
+    NumVol VARCHAR(6) NOT NULL,	
+    NumAv int(6) NOT NULL,
+    NumPil varchar(5) NOT NULL,
+    Ville_depart varchar(15) NOT NULL,
+    Ville_arrive varchar(15) NOT NULL,
+    Heure_dep time NOT NULL,
+    Heure_arrivee time NOT NULL
 );
 
-INSERT into vols (numero_vols, code_vol, id_pilote, ville_depart, ville_arrive, temp_depart, temp_arrive) values 
-('AF351', '152010', '00001', 'Paris', 'New York', '09:00', '17:00'), 
+INSERT into vols (NumVol, NumAv, NumPil, Ville_depart, Ville_arrive, Heure_dep, Heure_arrivee) values 
+('AF351', '152010', '00001', 'Paris', 'New York', '05:00', '23:00'), 
 ('RU154', '458950', '00002', 'Montreal', 'Londres', '13:00', '22:00'), 
 ('LH1792', '538710', '00003', 'Paris', 'Tunis', '11:00', '21:00');
-			
+
+
+/* table pilotes */
 CREATE TABLE pilotes (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    nom_pilote VARCHAR(15) NOT NULL,
-    prenom_pilote VARCHAR(15) NOT NULL,
-    code_pilote VARCHAR(5) NOT NULL,
-    ville_origine_pilote VARCHAR(20) NOT NULL,
-    distance_pilote INT(7) NOT NULL
+    NomPil VARCHAR(20) NOT NULL,
+    NumPil VARCHAR(5) NOT NULL,
+    Adresse VARCHAR(15) NOT NULL,
+    Salaire INT(6) NOT NULL
 );
 
-INSERT into pilotes (nom_pilote, prenom_pilote, code_pilote, ville_origine_pilote, distance_pilote) values 
-('Déodat', 'Clothilde', '00001', 'Paris', '650000'), 
-('Lisette', 'Ruben', '00002', 'Marseille', '825000'), 
-('Pascale', 'Hadrien', '00003', 'Cergy', '675000');
+INSERT into pilotes (NomPil, NumPil, Adresse, Salaire) values 
+('Déodat Clothilde', '00001', 'Paris', '650000'), 
+('Lisette Ruben', '00002', 'Marseille', '825000'), 
+('Pascale Hadrien', '00003', 'Cergy', '675000');
 
-alter table vols add constraint fk_pilote_id foreign key (id_pilote) references client(code_pilote);
+/* Donnez toutes les informations sur les pilotes de la compagnie. */
+SELECT * from pilotes;
+
+/* Quels sont les numéros des pilotes en service et les villes de départ de leurs vols ? */
+SELECT NumPil, Ville_depart from vols;
+
+/* Donnez la liste des avions dont la capacité est supérieure à 350 passagers. */
+SELECT * from avions WHERE Capacité>350;
+
+/* Quels sont les numéros et noms des avions localisés à 'Paris' ? */
+SELECT NumAv, NomAv from avions WHERE Localisation='Paris';
+
+/* Dans combien de villes distinctes sont localisées des avions ? */
+SELECT COUNT(DISTINCT Localisation) from avions;
+
+/* Quel est le nom des pilotes domiciliés à Cergydont le salaire est supérieur à 500000? */
+SELECT NomPil from pilotes WHERE Adresse='Cergy' and Salaire>'500000';
+
+/* Quels sont les avions (numéro et nom) localisés à Paris ou dont la capacité est inférieure à 350 passagers ? */
+SELECT NumAv, NomAv FROM avions WHERE Localisation='Paris' OR Capacité<'350';
+
+/* Liste des vols au départ de Paris allant à 'New york' après 18 heures ? */
+SELECT * from vols where TIMEDIFF(Heure_arrivee, Heure_dep)>='18';
+
+/* Quels sont les vols (numéro, ville de départ) effectués par les pilotes de numéro 00001et 00002? */
+SELECT NumVol, Ville_depart from vols WHERE NumPil='00001' or  NumPil='00002';
+
+/* Quels sont les pilotes dont le nom commence par «P» ? */
+SELECT * from pilotes WHERE NomPil LIKE 'P%';
+
+/* Quels sont les pilotes dont la troisième lettre est un «s» ? */alter
+SELECT * from pilotes WHERE NomPil LIKE '__s%';
+
+/* Quels sont les numéros des pilotes qui ne sont pas en service ? */
+
+/* ??? */
+
+
+
+
+
+
+
+
+
 
 /* afficher toutes les informations sur les pilotes de compagnie */
 SELECT DISTINCT
@@ -38,7 +99,7 @@ SELECT DISTINCT
     prenom_pilote,
     ville_origine_pilote,
     distance_pilote,
-    numero_vols,
+    NumVols,
     code_vol,
     ville_depart,
     ville_arrive,
@@ -59,6 +120,7 @@ FROM
     vols AS v
 WHERE
     p.code_pilote = v.id_pilote
+
 
 
 
