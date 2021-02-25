a007d5b5cddf16c2581005f91a83d52fba63df69
bakedpotato191 <vladislavp@pm.me>
2021-02-25 11:59:47 +0100

    Envoyer l'exercice pilotes et vols'

--- mysql/Fevrier/25-02/vols_pilotes.sql
+++ mysql/Fevrier/25-02/vols_pilotes.sql
@@ -11,9 +11,9 @@ CREATE TABLE vols (
     temp_arrive TIME NOT NULL
 );
 
-INSERT into vols (numero_vols, code_vol, id_pilote, ville_depart, ville_arrive, temp_depart, temp_arrive) values
-('AF351', '152010', '00001', 'Paris', 'New York', '09:00', '17:00'),
-('RU154', '458950', '00002', 'Montreal', 'Londres', '13:00', '22:00'),
+INSERT into vols (numero_vols, code_vol, id_pilote, ville_depart, ville_arrive, temp_depart, temp_arrive) values 
+('AF351', '152010', '00001', 'Paris', 'New York', '09:00', '17:00'), 
+('RU154', '458950', '00002', 'Montreal', 'Londres', '13:00', '22:00'), 
 ('LH1792', '538710', '00003', 'Paris', 'Tunis', '11:00', '21:00');
 			
 CREATE TABLE pilotes (
@@ -25,10 +25,10 @@ CREATE TABLE pilotes (
     distance_pilote INT(7) NOT NULL
 );
 
-INSERT into pilotes (nom_pilote, prenom_pilote, code_pilote, ville_origine_pilote, distance_pilote) values
-				 ('Déodat', 'Clothilde', '00001', 'Paris', '650000'),
-                 ('Lisette', 'Ruben', '00002', 'Marseille', '825000'),
-				 ('Pascale', 'Hadrien', '00003', 'Cergy', '675000');
+INSERT into pilotes (nom_pilote, prenom_pilote, code_pilote, ville_origine_pilote, distance_pilote) values 
+('Déodat', 'Clothilde', '00001', 'Paris', '650000'), 
+('Lisette', 'Ruben', '00002', 'Marseille', '825000'), 
+('Pascale', 'Hadrien', '00003', 'Cergy', '675000');
 
 alter table vols add constraint fk_pilote_id foreign key (id_pilote) references client(code_pilote);
 
