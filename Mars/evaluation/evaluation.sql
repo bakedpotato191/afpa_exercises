182ea52628095f2e0865e12a231ff01e8a2cf3f2
bakedpotato191 <vladislavp@pm.me>
2021-03-05 12:32:49 +0100

    appliquer les corrections du matin

--- Mars/evaluation/evaluation.sql
+++ Mars/evaluation/evaluation.sql
@@ -82,7 +82,7 @@ SELECT * from logement limit 1;
 
 #3.4
 
-SELECT COUNT(*) from logement;
+SELECT COUNT(*) as 'Nombre de logements' from logement;
 
 #3.5
 
@@ -90,7 +90,7 @@ SELECT * from logement WHERE prix < 150000 order by prix asc;
 
 #3.6
 
-SELECT COUNT(*) from logement WHERE categorie LIKE 'location';
+SELECT COUNT(*) as 'Nombre de logement avec location' from logement WHERE categorie LIKE 'location';
 
 #3.7
 
@@ -98,7 +98,7 @@ SELECT DISTINCT ville from demande;
 
 #3.8
 
-SELECT COUNT(*), ville from logement group by ville;
+SELECT COUNT(*) as 'Nombre des logements', ville from logement where categorie like 'vente' group by ville;
 
 #3.9
 
@@ -110,11 +110,11 @@ SELECT idLogement from logement where superficie between 20 and 30;
 
 #3.11
 
-SELECT * from logement where prix in (SELECT MIN(prix) from logement);
+SELECT MIN(prix) as 'Min Prix Vente' from logement where categorie like 'vente';
 
 #3.12
 
-SELECT DISTINCT ville from logement where type='maison';
+SELECT DISTINCT ville from logement where type like 'maison' and categorie = 'vente';
 
 #3.13
 
@@ -124,24 +124,29 @@ UPDATE logement_agence SET frais='730' WHERE idLogement='00003';
 
 SELECT * from logement as l, logement_agence as la WHERE idAgence in (SELECT idAgence from agence WHERE nom LIKE 'seloger') and l.idLogement = la.idLogement;
 
+select * from logement as l inner join logement_agence as la on l.idLogement = la.idLogement inner join agence on agence.idAgence = la.idAgence where agence.nom = 'seloger';
+
 #3.15
 
-SELECT COUNT(*) from logement as l, logement_personne as lp WHERE lp.idLogement in (SELECT idLogement from logement where ville LIKE 'Paris') and l.idLogement = lp.idLogement;
+SELECT COUNT(*) as 'Nombre' from logement as l, logement_personne as lp WHERE lp.idLogement in (SELECT idLogement from logement where ville LIKE 'Paris') and l.idLogement = lp.idLogement;
 
 #3.16
 
 SELECT * from personne as p
-INNER JOIN (SELECT idPersonne from demande LIMIT 3) as d on p.idPersonne = d.idPersonne;
+INNER JOIN (SELECT * from demande WHERE categorie = 'vente' LIMIT 3) 
+as d on p.idPersonne = d.idPersonne;
 
 #3.17
 
 SELECT prenom, email FROM personne AS p
-INNER JOIN (SELECT * FROM demande WHERE ville LIKE 'Paris') AS d ON p.idPersonne = d.idPersonne;
+INNER JOIN (SELECT * FROM demande WHERE ville LIKE 'Paris' and categorie='location') 
+AS d ON p.idPersonne = d.idPersonne;
 
 #3.18 
 
-SELECT a.nom, b.frais FROM agence AS a
-INNER JOIN (SELECT idAgence, SUM(frais) AS frais FROM logement_agence GROUP BY idAgence) AS b ON a.idAgence = b.idAgence;
+SELECT a.nom, b.frais AS 'bénéfice' FROM agence AS a
+INNER JOIN (SELECT idAgence, SUM(frais) AS frais FROM logement_agence GROUP BY idAgence ORDER BY frais ASC) 
+AS b ON a.idAgence = b.idAgence;
 
 #3.19
 
@@ -153,22 +158,23 @@ WHERE l.idLogement = lp.idLogement AND lp.idPersonne = p.idPersonne;
 
 #3.20
 
-SELECT COUNT(*) FROM logement WHERE ville LIKE 'hugo';
+SELECT COUNT(*) AS 'nombre' FROM logement WHERE categorie LIKE 'vente' AND ville IN 
+(SELECT ville FROM demande AS d, personne AS p WHERE d.idPersonne = p.idPersonne AND p.prenom = 'Hoeger');
 
 
 ##4
 
 #4.1
 
-create user 'afpa' identified by 'passwordAfpa';
-create user 'cda314' identified by 'passwordCda314';
+create user 'afpa'@'localhost' identified by 'passwordAfpa';
+create user 'cda314'@'localhost' identified by 'passwordCda314';
 
 #4.2
 
-grant select, insert on immobilier.personne to 'cda314';
-grant select, insert on immobilier.logement to 'cda314';
+grant select, insert on immobilier.personne to 'afpa@localhost';
+grant select, insert on immobilier.logement to 'afpa@localhost';
 
 #4.3
 
-grant delete on immobilier.logement to 'afpa';
-grant delete on immobilier.demande to 'afpa';
+grant delete on immobilier.logement to 'cda314@localhost';
+grant delete on immobilier.demande to 'cda314@localhost';
